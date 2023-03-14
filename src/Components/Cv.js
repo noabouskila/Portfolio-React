b86ee73ee4395e88eaa8454df848a1eb8e0ebe8f
John Doe <noabouskil@gmail.com>
2023-03-14 18:15:21 +0100

    cvresponsive

--- src/Components/Cv.js
+++ src/Components/Cv.js
@@ -2,11 +2,11 @@ import React from 'react'
 
 export default function Cv() {
   return (
-    <div className='flex flex-col  mt-12 h-96 mx-auto '>
+    <div className='flex flex-col  mt-12 h-52  md:h-96 mx-auto '>
       <h3 className='italic text-xl font-semibold mb-6 text-center'> Mon CV Dev Front-end</h3>
       <a href='https://imagizer.imageshack.com/img922/8884/BWeLE6.jpg' >
-        <img className='peer rounded shadow-xl hover:bg-slate-700/50 hover:opacity-10 cursor:pointor absolute w-9/12 ml-32 object-cover object-top h-96' src='https://noabouskila.github.io/Portfolio-React/img/CVNOA2023.jpg' alt="CV NOA BOUSKILA BAROUKH 2023"/>
-        <span className='opacity-0 peer-hover:opacity-100 font-bold relative top-32  left-96 z-10 w-full '>Clickez pour visualiser.</span>
+        <img className='peer rounded shadow-xl hover:bg-slate-700/50 hover:opacity-10 cursor:pointor absolute w-9/12 ml-8 md:ml-32 object-cover object-top h-52 md:h-96' src='https://noabouskila.github.io/Portfolio-React/img/CVNOA2023.jpg' alt="CV NOA BOUSKILA BAROUKH 2023"/>
+        <span className='opacity-0 peer-hover:opacity-100 font-bold relative left-12 md:top-32  md:left-96 z-10 w-full text-sm md:text-xl'>Clickez pour visualiser.</span>
       </a>
     </div>
   )
