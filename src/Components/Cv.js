8dc085ee790b217484a21dc6d68d07bbb8988590
John Doe <noabouskil@gmail.com>
2023-03-14 15:22:17 +0100

    cvmodif

--- src/Components/Cv.js
+++ src/Components/Cv.js
@@ -2,10 +2,10 @@ import React from 'react'
 
 export default function Cv() {
   return (
-    <div className='bg-red-500 flex flex-col items-center my-12 '>
+    <div className=' flex flex-col items-center mt-12 h-20'>
       <h3 className='italic text-xl font-semibold mb-6'> Mon CV Dev React</h3>
       <a href='https://imagizer.imageshack.com/img922/8884/BWeLE6.jpg' >
-        <img className='rounded' src='img/CVNOA2023.jpg' alt="CV NOA BOUSKILA BAROUKH 2023"/>
+        <img className='rounded w-10/12 mx-auto shadow-xl' src='img/CVNOA2023.jpg' alt="CV NOA BOUSKILA BAROUKH 2023"/>
       </a>
     </div>
   )
