2fb2731d2ad635f242535559e628ec37563a820a
John Doe <noabouskil@gmail.com>
2023-03-14 01:27:39 +0100

    commitimg

--- src/Components/MyProjects.js
+++ src/Components/MyProjects.js
@@ -23,7 +23,7 @@ export default function MyProjects() {
                 <img
                   alt="gallery"
                   className="absolute inset-0 w-full h-full object-cover object-center rounded-3xl shadow-2xl h-60"
-                  src={'process.env.PUBLIC_URL/'+project.image}
+                  src={process.env.PUBLIC_URL/+project.image}
                 />
                 <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-800 bg-gray-900 opacity-0 hover:opacity-100 h-60 rounded-3xl">
                   <h2 className="tracking-widest text-sm title-font font-medium text-green-400 mb-1">
